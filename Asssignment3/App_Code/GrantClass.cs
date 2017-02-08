1939eeac6a7db4e87d1843dfdd8ab643a3a2980f
Ariton84 <vanver@gmail.com>
2017-02-08 10:01:11 -0800

    Add files via upload

---
+++ Asssignment3/App_Code/GrantClass.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; //these libraries must be added
+using System.Data.SqlClient; //sql server tools
+using System.Configuration; //to talk to the config file
+
+
+
+/// <summary>
+/// Summary description for GrantClass
+/// </summary>
+public class GrantClass
+{
+    private SqlConnection connect;
+   
+    public GrantClass()
+    {
+        //get the connection string
+
+        string connectString =
+
+            ConfigurationManager.ConnectionStrings["GrantReviewConnection"].ToString();
+
+        //initilize the connection object
+
+        connect = new SqlConnection(connectString);
+    }
+    public DataTable Getgrants(int GrantKey)
+
+    {
+
+        //declare a new data table to store the results
+
+        DataTable table = new DataTable();
+
+        //sql statement
+
+        string sql = "Select GrantReviewDate as [Date], "
+                + "GrantRequestExplanation as Explanation, "
+                + "GrantAllocationAmount as Amount, "
+                + "GrantRequestStatus as [Status] "
+                + "From GrantRequest r "
+                + "inner join GrantReview gr "
+                + "on r.GrantRequestKey = gr.GrantRequestKey "
+                + " Where GrantTypeKey = @GrantTypeKey";
+
+        //the command object passes the sql through the connection
+
+        SqlCommand cmd = new SqlCommand(sql, connect);
+
+        //the parameter replaces the sql variable with a value
+
+        cmd.Parameters.AddWithValue("@GrantTypeKey", GrantKey);
+
+        //declare a reader to stream the results
+
+        SqlDataReader reader = null;
+
+        //open the connection
+
+        connect.Open();
+
+        //execute the command and reader
+
+        reader = cmd.ExecuteReader();
+
+        //load the results into the table 
+
+        table.Load(reader);
+
+        //close stuff
+
+        reader.Close();
+
+        connect.Close();
+
+        return table;
+
+    }
+
+
+
+    public DataTable GetGrants()
+
+    {
+
+        DataTable table = new DataTable();
+
+        string sql = "Select GrantTypeKey, GrantTypeName from GrantType";
+
+        SqlCommand cmd = new SqlCommand(sql, connect);
+
+        SqlDataReader reader = null;
+
+        connect.Open();
+
+        reader = cmd.ExecuteReader();
+
+        table.Load(reader);
+
+        reader.Close();
+
+        connect.Close();
+
+        return table;
+
+    }
+
+}
---
+++ Asssignment3/App_Code/LoginClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for LoginClass
+/// </summary>
+public class LoginClass
+{
+    SqlConnection connect;
+    string user;
+    string pass;
+    public LoginClass(string userName, string password)
+    {
+        user = userName;
+        pass = password;
+
+        string connectString = ConfigurationManager.ConnectionStrings["GrantReviewConnection"].ToString();
+        connect = new SqlConnection(connectString);
+    }
+    public int ValidateLogin()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = connect;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "usp_Login";
+        cmd.Parameters.AddWithValue("@email", user);
+        cmd.Parameters.AddWithValue("@password", pass);
+
+        connect.Open();
+
+        int result = cmd.ExecuteNonQuery();
+
+        connect.Close();
+
+        int key = 0;
+
+        if(result != -1)
+        {
+            key = GetUserKey();
+        }
+        return key;
+    }
+
+    private int GetUserKey()
+    {
+        string sql = "Select PersonKey from person where PersonEmail =@user";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@user", user);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        int key = 0;
+        while (reader.Read())
+        {
+            key = (int)reader["PersonKey"];
+        }
+        connect.Close();
+
+        return key;
+    }
+}
